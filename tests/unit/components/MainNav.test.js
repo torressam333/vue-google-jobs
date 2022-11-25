1e297320fd032a87fc43a4f25c51a008eda04c83
Samuel Torres <sam.torres@smiledirectclub.com>
2022-11-25 08:21:34 -0800

    Add unit test for main nav navigation

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -11,4 +11,20 @@ describe('MainNav', () => {
   it('Displays the correct company name', () => {
     expect(wrapper.text()).toContain('Torres Careers');
   });
+
+  it('Displays menu items for navigation', () => {
+    // Find all list items
+    const navigationMenuItems = wrapper.findAll('li');
+    const navMenuItemsText = navigationMenuItems.map((item) => item.text());
+
+    expect(navigationMenuItems.length).toBe(6);
+    expect(navMenuItemsText).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Torres Co',
+      'How we hire',
+      'Students',
+      'Jobs',
+    ]);
+  });
 });

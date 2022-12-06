19f8d3d3433c29802fb70e124404872e7646bbb1
Samuel <torressam333@gmail.com>
2022-12-06 08:28:08 -0700

    Add additional custom css to action button

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white"
+    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
   >
     {{ buttonText }}
   </button>

d8a8b1492f828cc876ac43ffb430964e0d7e9122
Samuel <torressam333@gmail.com>
2022-12-07 06:19:01 -0700

    Working on text fix for action button

--- tests/unit/components/ActionButton.test.js
+++ tests/unit/components/ActionButton.test.js
@@ -4,5 +4,9 @@ import ActionButton from "@testing-library/vue";
 describe("ActionButton", () => {
   it("displays text passed in as a prop", () => {
     render(ActionButton);
+
+    const buttonText = screen.getByText("Submit");
+
+    expect(buttonText).toBe("Submit");
   });
 });

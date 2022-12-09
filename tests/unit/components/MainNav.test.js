5316cf62393e0da5534d829ea81a6742dee7a023
Samuel <torressam333@gmail.com>
2022-12-08 19:09:01 -0700

    Write test to simulate user log in and log out

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
@@ -28,21 +29,40 @@ describe("MainNav", () => {
   });
 
   describe("when the user logs in", () => {
-    it("displays user profile image", () => {
+    it("displays user profile image", async () => {
       render(MainNav);
 
       // Search for image by alt text regex
-      const profileImage = screen.queryByRole("img", {
+      let profileImage = screen.queryByRole("img", {
         name: /profile image/i,
       });
 
-      expect(profileImage).not.toBeInTheDocument();
-
-      const loginButton = screen.getByRole("button", {
+      let loginButton = screen.getByRole("button", {
         name: /sign in/i,
       });
 
-      //
+      // Login text exists initially
+      expect(loginButton).toBeInTheDocument();
+      expect(profileImage).not.toBeInTheDocument();
+
+      // Simulate click to log user in
+      await userEvent.click(loginButton);
+
+      // Have to re query the document
+      profileImage = screen.queryByRole("img", {
+        name: /profile image/i,
+      });
+
+      expect(profileImage).toBeInTheDocument();
+      expect(loginButton).not.toBeInTheDocument();
+
+      // Simulate logout
+      await userEvent.click(profileImage);
+
+      loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+
       expect(loginButton).toBeInTheDocument();
     });
   });
